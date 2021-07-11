39fe3584f424f270cfcbe1ab0a6fe49c528c760d
mido.hamdy.142 <you@example.com>
2021-07-11 23:20:17 +0300

    first commit4

--- js/main.js
+++ js/main.js
@@ -42,6 +42,8 @@ $(document).ready(function () {
     });
     //client slider
     $('.client .owl-carousel').owlCarousel({
+        touchDrag: false,
+        mouseDrag: false,
         loop:false ,
         margin:15,
         nav:true,
